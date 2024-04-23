a8c509a764af824617d6284031aef06e17854322
elvin morales <elvinmorales.hernandez96@gmail.com>
2024-04-23 10:32:26 -0600

    rutas y archivos

---
+++ informacion/urls.py
@@ -0,0 +1,11 @@
+#crear mi rutas
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    #inicio es el nombre de la ruta
+    path('',views.inicio, name='inicio'),
+]
+
+
+
--- webtrafico/urls.py
+++ webtrafico/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+#inclui la url de mi aplicacion informativo
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #agregar mis librerias
+    path('',include('informacion.urls')),
 ]
